4a9ac482cd8b58faf235dd5643d2f9e9176c1655
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-28 14:58:55 +0100

    commit de sauvegarde

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,14 @@ before_action :authenticate_user!
     @task = Task.new(task_params)
     @category = Category.find(category_params)
     @task.category = @category
+
     if @task.save
-      redirect_to root_path
-      flash[:notice] = "Task created"
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+      # redirect_to root_path
+      # flash[:notice] = "Task created"
     else
       redirect_to root_path
       flash[:notice] = "Please try again"
