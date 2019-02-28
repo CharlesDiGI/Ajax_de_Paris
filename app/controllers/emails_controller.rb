3382623a2bb86f4e10e348e62a56d7b334befa99
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-28 17:09:03 +0100

    ajout fonction show et delete emails

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,30 @@
+class EmailsController < ApplicationController
+  def index
+    @emails = Email.all
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    @emails=Email.all # needed for sidebar, probably better to use a cell for this
+    @email.update_attribute(:read, [true])
+        respond_to do |format|
+        format.html # show.html.erb
+        format.js {render layout: false}
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @emails = Email.all
+    Email.find(params[:id].to_i).destroy
+    respond_to do |format|
+      format.js {render layout: false}
+    end
+  end
+
+end
